111431956b3b65968ed2ac2cbb301f1d324fab08
Original Timi <timileyinoyelekan11@gmail.com>
2024-11-13 13:45:49 +0100

    fix:

--- src/utils/createCategory.ts
+++ src/utils/createCategory.ts
@@ -1,10 +1,9 @@
 import Category from "../models/Category";
 
-export default (categories:string /** SEPERATED BY COMMA **/)=>{
-    const category = categories.trim().split(",");
-    category.map(async(ca)=>{
-        const CategoryDB = await Category.bulkCreate({
-            
-        }) 
-    })
-}
+export default (categories: string /** SEPERATED BY COMMA **/) => {
+  // const category = categories.trim().split(",");
+  // category.map(async(ca)=>{
+  //     const CategoryDB = await Category.bulkCreate({
+  //     })
+  // })
+};
